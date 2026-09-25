a6e4f1d13fb3d6452e5bf623362a131dc11f6788
Abdiqadiribra <abdiqadiribra1@gmail.com>
2026-09-25 08:44:46 +0300

    Add files via upload

---
+++ ASSIGNMENT/test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i= 1; $i<=3;$i++) //row loop
+        for ($j=1;$j<=5;$j++) //column loop
+        echo("Row is $i and column $j result is = "($i * $j) . "<br>");
+    ?>
+</body>
+</html>
